6fbcf5faad8fbb1c34c91e682813380ae8f86001
Nicholas <79084434+Jurv1@users.noreply.github.com>
2023-02-09 18:39:10 +0300

    reworked findEl.ts

--- src/utils/findEl.ts
+++ src/utils/findEl.ts
@@ -6,7 +6,7 @@ import {Blog} from "../schemas/blogSchemas";
 export default (req: Request, res: Response, collection: Video[]|Post[]|Blog[] ) => {
     const id = req.params.id
     // @ts-ignore
-    const foundedEl = collection.find(el => el?.id === +id)
+    const foundedEl = collection.find(el => el?.id == id)
     if (foundedEl) {
         res.status(200).send(foundedEl)
         return;
