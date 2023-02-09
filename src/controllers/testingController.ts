598efd87ed1eb3aa479bfbedec4c62d2deab0409
Nicholas <79084434+Jurv1@users.noreply.github.com>
2023-02-09 15:54:17 +0300

    added testing valid

--- src/controllers/testingController.ts
+++ src/controllers/testingController.ts
@@ -6,13 +6,18 @@ import {posts} from "./postController";
 
 export const deleteAll = async (req: Request, res: Response) => {
     try {
-        // @ts-ignore
-        videos = []
-        // @ts-ignore
-        blogs = []
-        // @ts-ignore
-        posts = []
-        res.send(204)
+        if(videos.length === 0 && blogs.length === 0 && posts.length === 0) return res.status(204)
+            .json({"EW": "WE"})
+        else {
+            // @ts-ignore
+            videos = []
+            // @ts-ignore
+            blogs = []
+            // @ts-ignore
+            posts = []
+            res.send(204).json({"EW": "WE"})
+        }
+
     } catch (err) {
         console.log(err)
         res.status(404).json({
