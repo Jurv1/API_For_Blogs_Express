76682f70a3e292257ac433fe8bae1c4ab81b64dc
Nicholas <79084434+Jurv1@users.noreply.github.com>
2023-02-10 03:43:44 +0300

    fixed Validators

--- src/validations/blogValidator.ts
+++ src/validations/blogValidator.ts
@@ -1,8 +1,12 @@
 import { body } from "express-validator";
 
 export const blogValidation = [
-    body('name').trim().isString().isLength({ min: 1, max: 15}),
-    body('description').trim().isString().isLength({min: 1, max: 500}),
-    body('websiteUrl').matches('^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$')
+    body('name').exists().bail()
+        .trim().isLength({ min: 1, max: 15}).bail()
+            .isString(),
+    body('description').exists().bail()
+        .trim().isLength({min: 1, max: 500}).bail()
+            .isString(),
+    body('websiteUrl').exists().matches('^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$')
         .isLength({max: 100})
 ]
--- src/validations/postValidator.ts
+++ src/validations/postValidator.ts
@@ -1,9 +1,16 @@
 import { body } from "express-validator";
 
 export const postValidation = [
-    body('title').trim().isString().isLength({min: 1, max: 30}),
-    body('shortDescription').trim().isString().isLength({min: 1, max: 100}),
-    body('content').trim().isString().isLength({min: 1, max: 1000}),
-    body('blogId').trim().isString(),
+    body('title').exists().bail()
+        .trim().isLength({min: 1, max: 30}).bail()
+            .isString(),
+    body('shortDescription').exists().bail()
+        .trim().isLength({min: 1, max: 100}).bail()
+            .isString(),
+    body('content').exists().bail()
+        .trim().isLength({min: 1, max: 1000}).bail()
+            .isString(),
+    body('blogId').exists().bail()
+        .trim().isString(),
     body('blogName').trim().optional().isString()
 ]
