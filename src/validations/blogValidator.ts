69970d17e57c11c6588ae7e5644ab23535a4c18e
Nicholas <79084434+Jurv1@users.noreply.github.com>
2023-02-10 03:12:55 +0300

    fixed Validators

--- src/validations/blogValidator.ts
+++ src/validations/blogValidator.ts
@@ -3,6 +3,5 @@ import { body } from "express-validator";
 export const blogValidation = [
     body('name').trim().isString().isLength({ min: 1, max: 15}),
     body('description').trim().isString().isLength({min: 1, max: 500}),
-    body('websiteUrl').isLength({ min: 1, max: 100 })
-        .matches('^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$')
+    body('websiteUrl').matches('^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$')
 ]
