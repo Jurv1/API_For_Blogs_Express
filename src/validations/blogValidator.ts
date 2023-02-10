35f28c9e1a49062e45f2559b64c7716dc24f9700
Nicholas <79084434+Jurv1@users.noreply.github.com>
2023-02-10 03:17:46 +0300

    fixed Validators

--- src/validations/blogValidator.ts
+++ src/validations/blogValidator.ts
@@ -4,4 +4,5 @@ export const blogValidation = [
     body('name').trim().isString().isLength({ min: 1, max: 15}),
     body('description').trim().isString().isLength({min: 1, max: 500}),
     body('websiteUrl').matches('^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$')
+        .isLength({max: 100})
 ]
