2c68967a893e461d98d2f15b42e1bbe09ae65a45
Nicholas <79084434+Jurv1@users.noreply.github.com>
2023-02-17 01:11:57 +0300

    added isMembership and createdAt to blogs and posts

--- src/validations/blogValidator.ts
+++ src/validations/blogValidator.ts
@@ -8,5 +8,7 @@ export const blogValidation = [
         .trim().isLength({min: 1, max: 500}).bail()
             .isString(),
     body('websiteUrl').exists().matches('^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$')
-        .isLength({max: 100})
+        .isLength({max: 100}),
+    body('createdAt').matches('/\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/'),
+    body('isMembership').isBoolean()
 ]
