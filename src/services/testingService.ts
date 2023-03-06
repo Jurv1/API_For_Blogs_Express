8eb1fa5904ca36c6031c6e48c17c08be2ae7d137
Nicholas <79084434+Jurv1@users.noreply.github.com>
2023-03-06 21:01:09 +0300

    lil fix

--- src/services/testingService.ts
+++ src/services/testingService.ts
@@ -1,11 +1,12 @@
 import {Request, Response} from "express";
-import {blogDBController, postDBController, videoDBController} from "../db/db";
+import {blogDBController, postDBController, userDBController, videoDBController} from "../db/db";
 
 export const deleteAll = async (req: Request, res: Response) => {
     try {
         await postDBController.deleteMany({})
         await videoDBController.deleteMany({})
         await blogDBController.deleteMany({})
+        await userDBController.deleteMany({})
         res.sendStatus(204)
     } catch (err) {
         console.log(err)
