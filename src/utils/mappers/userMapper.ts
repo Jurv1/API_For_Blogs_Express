a77b27e885da611443d4227e67f0edf7bc245f33
Nicholas <79084434+Jurv1@users.noreply.github.com>
2023-03-22 20:12:38 +0300

    edited userSchema

--- src/utils/mappers/userMapper.ts
+++ src/utils/mappers/userMapper.ts
@@ -3,7 +3,6 @@ import {ViewUserModel} from "../../schemas/presentationSchemas/userSchemas";
 
 export function mapUser(obj: FinalDBUser): ViewUserModel{
     return {
-
         id: obj._id.toString(),
         login: obj.accountData.login,
         email: obj.accountData.email,
@@ -14,12 +13,10 @@ export function mapUser(obj: FinalDBUser): ViewUserModel{
 export function mapUsers(objs: FinalDBUser[]): ViewUserModel[]{
     return objs.map(el => {
         return {
-
             id: el._id.toString(),
             login: el.accountData.login,
             email: el.accountData.email,
             createdAt: el.accountData.createdAt
-
         }
     })
 }
