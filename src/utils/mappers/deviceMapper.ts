77bc25c89e51018c76bc6ed6f2c4cf1e73c7687b
Nicholas <79084434+Jurv1@users.noreply.github.com>
2023-03-29 16:05:07 +0300

    fixed mapper

--- src/utils/mappers/deviceMapper.ts
+++ src/utils/mappers/deviceMapper.ts
@@ -5,7 +5,7 @@ export function mapDevice(obj: FinalDBDevice): viewDeviceModel{
     return {
         ip: obj.ip,
         title: obj.title,
-        lastActivity: new Date(obj.lastActivity.toString()) ,
+        lastActivity: new Date(obj.lastActivity) ,
         deviceId: obj.deviceId,
     }
 }
@@ -16,7 +16,7 @@ export function mapDevices(objs: FinalDBDevice[]): viewDeviceModel[]{
 
             ip: el.ip,
             title: el.title,
-            lastActivity: new Date(el.lastActivity.toString()) ,
+            lastActivity: new Date(el.lastActivity) ,
             deviceId: el.deviceId,
 
         }
